50a99b11a3062d874aad22883c3254cb6462e207
josbert2 <hernandezjosbert@gmail.com>
2024-12-08 16:37:21 -0300

    🐛 fix(minor)

--- apps/web/src/sass/base/Rounded.js
+++ apps/web/src/sass/base/Rounded.js
@@ -2,5 +2,11 @@
   export const borderRadius = {
     '10': '.625rem',
     '20': '1.25rem',
+
+    // Primary styles
+    xl: 'calc(var(--radius) + 4px)',
+    lg: 'var(--radius)',
+    md: 'calc(var(--radius) - 2px)',
+    sm: 'calc(var(--radius) - 4px)',
   }
   
